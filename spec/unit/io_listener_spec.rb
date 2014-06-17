774ce34a7f1d3241940153072cfda2b435032e3e
Oliver Martell <oliver.martell@gmail.com>
2014-06-18 00:53:47 +0100

    Let's use io_listener instead

--- spec/unit/io_listener_spec.rb
+++ spec/unit/io_listener_spec.rb
@@ -9,19 +9,19 @@ module ActivityBroker
     end
 
     let!(:fake_listener) { FakeListener.new }
-    let!(:subject) { IOListener.new(fake_listener, :forwarding_follow_event) }
+    let!(:io_listener) { IOListener.new(fake_listener, :forwarding_follow_event) }
 
     it 'is equal to other io listener when listener and event match' do
-      expect(subject).to eq IOListener.new(fake_listener, :forwarding_follow_event)
+      expect(io_listener).to eq IOListener.new(fake_listener, :forwarding_follow_event)
     end
 
     it 'is not equal to another io listener when event does not match' do
-      expect(subject).not_to eq IOListener.new(fake_listener, :forwarding_status_update)
+      expect(io_listener).not_to eq IOListener.new(fake_listener, :forwarding_status_update)
     end
 
     it 'is not equal to anoother io listener when registered listener does not match' do
       another_fake_listener = FakeListener.new
-      expect(subject).not_to eq IOListener.new(another_fake_listener, :forwarding_follow_event)
+      expect(io_listener).not_to eq IOListener.new(another_fake_listener, :forwarding_follow_event)
     end
   end
 end
