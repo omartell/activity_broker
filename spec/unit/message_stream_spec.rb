870b20b53f1079c0e88030bf019b35847368ae00
Oliver Martell <oliver.martell@gmail.com>
2014-06-22 19:03:38 +0100

    Deregister listeners when there's no more interest in updates

--- spec/unit/message_stream_spec.rb
+++ spec/unit/message_stream_spec.rb
@@ -18,30 +18,29 @@ module ActivityBroker
         @listener = IOListener.new(listener, event, stop_event)
       end
 
-      def deregister_write(listener, event)
+      def deregister_write(listener, event); end
 
-      end
+      def deregister_read(listener, event); end
     end
 
     let!(:event_loop) { FakeEventLoop.new  }
     let!(:fake_io) { double }
     let!(:message_stream){ MessageStream.new(fake_io, event_loop, double.as_null_object) }
+    let!(:message_listener) { double(:process_message) }
+    let!(:one_message) { '1|U|sender|recipient' }
 
     it 'buffers messages from event source and streams them to the message listener' do
-      fake_io.stub(read_nonblock: '1|U|sender|recipient' + MessageStream::CRLF + '2|U|sender')
-
-      message_listener = double(:process_message)
+      fake_io.stub(read_nonblock: "1|U|sender|recipient" + MessageStream::CRLF + "2|U|sender")
 
       message_stream.read(message_listener)
 
-      expect(message_listener).to receive(:process_message).with('1|U|sender|recipient', message_stream)
+      expect(message_listener).to receive(:process_message)
+        .with(one_message, message_stream)
 
       event_loop.notify_read_event
     end
 
     it 'buffers outgoing messages and writes them in the right format' do
-      one_message = '1|U|sender|recipient'
-
       # Try to write 3 messages
       message_stream.write(one_message)
       message_stream.write(one_message)
@@ -60,6 +59,26 @@ module ActivityBroker
       expect(fake_io).to receive(:write_nonblock)
         .with(expected_message)
         .and_return(expected_message.bytesize) # Check if we get the last message
+
+      event_loop.notify_write_event
+    end
+
+    it 'deregisters read listener when there\'s no more data coming from the other end' do
+      fake_io.stub(:read_nonblock) { raise EOFError }
+
+      message_stream.read(message_listener)
+
+      expect(event_loop).to receive(:deregister_read).with(message_stream, :data_received)
+
+      event_loop.notify_read_event
+    end
+
+    it 'deregisters write listener when there\'s nothing left to write' do
+      fake_io.stub(write_nonblock: (one_message + MessageStream::CRLF).bytesize)
+
+      message_stream.write(one_message)
+
+      expect(event_loop).to receive(:deregister_write).with(message_stream, :ready_to_write)
 
       event_loop.notify_write_event
     end
