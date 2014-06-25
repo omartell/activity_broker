8cb5ea001d54b9672127865b274089191ab74b1b
Oliver Martell <oliver.martell@gmail.com>
2014-06-25 08:59:49 +0100

    Improve test descriptions for io listener

--- spec/unit/io_listener_spec.rb
+++ spec/unit/io_listener_spec.rb
@@ -11,15 +11,15 @@ module ActivityBroker
     let!(:fake_listener) { FakeListener.new }
     let!(:io_listener) { IOListener.new(fake_listener, :forwarding_follow_event) }
 
-    it 'is equal to other io listener when listener and event match' do
+    it 'is equal to another io listener when listener and event match' do
       expect(io_listener).to eq IOListener.new(fake_listener, :forwarding_follow_event)
     end
 
-    it 'is not equal to another io listener when event does not match' do
+    it 'is not equal to another io listener when registered events dont match' do
       expect(io_listener).not_to eq IOListener.new(fake_listener, :forwarding_status_update)
     end
 
-    it 'is not equal to anoother io listener when registered listener does not match' do
+    it 'is not equal to another io listener when registered listeners dont match' do
       another_fake_listener = FakeListener.new
       expect(io_listener).not_to eq IOListener.new(another_fake_listener, :forwarding_follow_event)
     end
