474cacbabbe381dab0af1c952424201ef363f891
Oliver Martell <oliver.martell@gmail.com>
2014-06-29 14:31:20 +0100

    Move event logger

---
+++ lib/activity_broker/application_event_logger.rb
@@ -0,0 +1,77 @@
+require 'logger'
+module ActivityBroker
+  class ApplicationEventLogger
+    def initialize(output, level)
+      @logger = Logger.new(output)
+      @logger.datetime_format = "%Y-%m-%d %H:%M:%S"
+      @logger.level = level
+    end
+
+    def log(event, *other)
+      send(event, *other)
+    end
+
+    private
+
+    def log_debug(message)
+      @logger.debug(message)
+    end
+
+    def log_info(message)
+      @logger.info(message)
+    end
+
+    def starting_activity_broker
+      log_info('=======================================================')
+      log_info('Starting Activity Broker')
+    end
+
+    def server_accepting_connections(port)
+      log_info('TCP Server accepting connections on port ' + port.to_s)
+    end
+
+    def connection_accepted(port)
+      log_debug('connection accepted on port ' + port.to_s)
+    end
+
+    def streaming_message(message)
+      log_debug('streaming message ' + message)
+    end
+
+    def registering_subscriber(subscriber_id)
+      log_debug('registering subscriber ' + subscriber_id)
+    end
+
+    def discarding_unfollow_event(message)
+      log_debug('discarding unfollow event: ' + message)
+    end
+
+    def forwarding_broadcast_event(notification)
+      log_debug('forwarding broadcast event: ' + notification.message)
+    end
+
+    def forwarding_follow_event(notification)
+      log_debug('forwarding follow event: ' + notification.message)
+    end
+
+    def forwarding_unfollow_event(notification)
+      log_debug('forwarding unfollow event: ' + notification.message)
+    end
+
+    def forwarding_status_update(notification)
+      log_debug('forwarding status update: ' + notification.message)
+    end
+
+    def forwarding_private_message(notification)
+      log_debug('forwarding private message: ' + notification.message)
+    end
+
+    def stopping_server(port)
+      log_debug('stopping server on port ' + port.to_s)
+    end
+
+    def stopping_event_loop
+      log_info('stopping activity broker')
+    end
+  end
+end
