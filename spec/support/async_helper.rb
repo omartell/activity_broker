e11be38ce5565e3c2e314817fcc9d297bcd32e84
Oliver Martell <oliver.martell@gmail.com>
2014-06-29 20:15:34 +0100

    Add docs to test helpers

--- spec/support/async_helper.rb
+++ spec/support/async_helper.rb
@@ -1,13 +1,19 @@
 module AsyncHelper
+
+  # Instead of waiting an specific an amount of time and then
+  # asserting for a certain behaviour, this test helper polls for an assertion
+  # success every X number of seconds (configurable as interval).
+  # The test will fail if the assertion doesn't pass after Y number of
+  # seconds (configurable as timeout).
   def eventually(options = {})
-    timeout    = options[:timeout]   || 1
-    interval   = options[:interval] || 0.0001
+    timeout    = options[:timeout] || 1 # seconds
+    interval   = options[:interval] || 0.0001 # seconds
     time_limit = Time.now + timeout
 
     loop do
       begin
         yield
-      rescue => error
+      rescue RSpec::Expectations::ExpectationNotMetError => error
 
       end
 
@@ -20,10 +26,13 @@ module AsyncHelper
     end
   end
 
+  # Same behaviour as eventually, but decided to change the method name to make
+  # it more clear that this is focused on 'synchronizing' the test with the application
   def wait_until(options = {}, &block)
     eventually(options, &block)
   end
 
+  # Small wrapper around sleep
   def on_timeout(options = {})
     timeout = options[:timeout]   || 0.5
 
